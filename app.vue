f88ab443afede98c02fb381853f580b2109c2687
Danielwinkelmann <dwinkelmann@outlook.com>
2023-03-02 10:03:40 +0100

    feat: test prefetchComponents

--- app.vue
+++ app.vue
@@ -2,10 +2,17 @@
 import type { DragDropComponents } from '#lenne-tech'
 const dragDropComponents: DragDropComponents[] = ['DragDropGrid', 'DragDropImage', 'DragDropRichText']
 const selectedComponent = ref<DragDropComponents>('DragDropGrid')
+
+const prefetch = async () => {
+  await prefetchComponents(dragDropComponents)
+}
 </script>
 
 <template>
-  <div class="min-h-screen w-full flex justify-center items-center flex-col space-y-20 max-w-3xl mx-auto">
+  <div class="min-h-screen w-full flex justify-center items-center flex-col space-y-12 max-w-3xl mx-auto">
+    <button class="px-2 py-1.5 bg-blue-400 active:bg-blue-600 hover:bg-blue-500 rounded-lg text-slate-100" @click="prefetch()">
+      prefetchComponents
+    </button>
     <ul class="flex gap-4">
       <li v-for="component of dragDropComponents" :key="component">
         <button class="px-2 py-1.5 bg-slate-400 active:bg-slate-600 hover:bg-slate-500 rounded-lg text-slate-100" @click="selectedComponent = component">
