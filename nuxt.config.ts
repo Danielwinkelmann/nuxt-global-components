1dfb26212e3b44c41be20ba78ec4b4d1f69e30d1
Danielwinkelmann <dwinkelmann@outlook.com>
2023-03-19 07:16:17 +0100

    feat: add runtime module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', './modules/drag-drop-module/module'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-monaco-editor', 'nuxt-runtime-compiler', './modules/drag-drop-module/module'],
 
   vite: {
-    resolve: {
+    /*     resolve: {
       alias: {
         vue: 'vue/dist/vue.esm-bundler.js',
       },
-    },
+    }, */
   },
 })
--- pages/render-vue-string.vue
+++ pages/render-vue-string.vue
@@ -1,5 +1,23 @@
 <script lang="ts" setup>
-const template = ref('<div><MyCustomComponent test="Test String" :num-one="3" :num-two="5" ></MyCustomComponent>This is some vue HTML <button @click="$emit(\'runFunction\')">Run Function</button></div>')
+const count = ref(1)
+
+const template = ref(`
+<div class="flex flex-col justify-start items-start">
+    <MyCustomComponent 
+        test="Test String" 
+        :num-one="2" 
+        :num-two="5"
+    >
+    </MyCustomComponent>
+    This is some vue HTML
+    <button @click="$emit(\'runFunction\')">
+        Run Function
+    </button>
+    <button class="p-2 bg-blue-200 rounded-sm" @click="$emit(\'addCount\')">
+        Add Count
+    </button>
+</div>
+`)
 const testFunction = () => {
   // eslint-disable-next-line no-console
   console.log('clicked')
@@ -8,12 +26,12 @@ const testFunction = () => {
 
 <template>
   <div class="container m-auto auto-rows-auto grid grid-cols-2 gap-4">
-    <div class="border-solid border-gray-500 rounded border p-2 h-screen">
-      ...Editor der die Variable template befuellt
-      <textarea id="" v-model="template" name="" cols="30" rows="10" />
+    <div class="border-solid border-gray-500 rounded border p-2 h-screen ">
+      ...Editor der die Variable template befuellt: {{ count }}
+      <MonacoEditor v-model="template" class="!h-1/2 !w-full" :options="{ theme: 'vs-dark' }" lang="html" />
     </div>
     <div class="border-solid border-gray-500 rounded border p-2">
-      <RenderVueString :html="template" @run-function="testFunction()" />
+      <RenderVueString :html="template" @add-count="++count" @run-function="testFunction()" />
     </div>
   </div>
 </template>
